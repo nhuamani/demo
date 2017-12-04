35b8c5f66664c1e696721ff7398962763b0cfa38
nhuamani <nhuamani.c7@gmail.com>
2017-12-03 19:53:00 -0500

    separacion en una pagina html

--- demo_web/app/app.js
+++ demo_web/app/app.js
@@ -1,3 +1,13 @@
 var myApp = angular.module("myApp", [
-
+    'ui.router'
     ]);
+
+myApp
+.config(function ($stateProvider){
+    $stateProvider
+
+    .state("pag1", {
+        url:"/pagina1",
+        templateUrl:"app/views/pagina.html"
+    });
+});
